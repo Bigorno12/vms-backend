69677d3d879529749018558d11bac1fc165a1159
pranav <pranav.seewoodharry@elca.mu>
2023-02-09 09:50:52 +0400

    Add QuerydslPredicateExecutor in GenericRepository

--- persistence/src/main/java/com/pacdesign/persistence/repository/generic/GenericRepository.java
+++ persistence/src/main/java/com/pacdesign/persistence/repository/generic/GenericRepository.java
@@ -1,9 +1,10 @@
 package com.pacdesign.persistence.repository.generic;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface GenericRepository<T> extends JpaRepository<T, Long> {
+public interface GenericRepository<T> extends JpaRepository<T, Long>, QuerydslPredicateExecutor<T> {
 
 }
